752c381b86d9b7d1cb235b4cb66a958c4c150526
Maximilian CG <mcgoeritz@gmail.com>
2023-07-18 08:37:10 -0300

    Implement menu and leaderboard

---
+++ components/Leaderboard.vue
@@ -0,0 +1,17 @@
+<template>
+	<ol>
+		<li v-for="score in leaderboard">
+			{{score["timestamp"]}}: {{score["score"]}}
+		</li>
+	</ol>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// change here to get leaderboard from server
+const leaderboard = ref([{"timestamp":1689371592,"score":8150.0},{"timestamp":1689675464,"score":4200.0},{"timestamp":1689678006,"score":400.0}])
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,31 @@
 <template>
-  <div id="canvas">
-    <div id="deep-space" />
-    <div id="space-field">
-      <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
-
-      <SpaceObject class="asteroid" :data="asteroid" resolution="2"
-                  :key="asteroid.center"
-                  v-for="asteroid in spaceField.asteroids" />
-
-      <SpaceObject class="missile" :data="missile" resolution="2"
-                  :key="missile.center"
-                  v-for="missile in spaceField.missiles" />
-
-      <SpaceObject class="explosion" :data="explosion" resolution="2"
-                  :key="explosion.center"
-                  v-for="explosion in spaceField.explosions" />
-    </div>
-  </div>
+	<template v-if="state == 'menu'">
+		<h3>Galaxy Raiders</h3>
+		<button @click="state = 'game'">Jogar</button>
+		<button @click="state = 'leaderboard'">Placar</button>
+		<button @click="close()">Fechar</button>
+	</template>
+	<template v-if="state == 'leaderboard'">
+		<Leaderboard />
+		<button @click="state = 'menu'">Back to menu</button>
+	</template>
+	<template v-if="state == 'game'">
+		<Game />
+	</template>
 </template>
 
 <script setup>
-const {
-  data: spaceField,
-  refresh: updateSpaceField
-} = await $get("/space-field");
+import { ref } from 'vue'
 
-onMounted(() => {
-  window.addEventListener("keydown", async (event) => {
-    const keyToCommand = {
-      "ArrowUp": "MOVE_SHIP_UP",
-      "ArrowDown": "MOVE_SHIP_DOWN",
-      "ArrowRight": "MOVE_SHIP_RIGHT",
-      "ArrowLeft": "MOVE_SHIP_LEFT",
-      "Space": "LAUNCH_MISSILE",
-      "Escape": "PAUSE_GAME",
-    };
+const state = ref('menu')
 
-    const command = keyToCommand[event.code];
+</script>
 
-    // Ignore if invalid key was pressed
-    if (command === undefined) return;
-
-    console.log(`Triggering command: ${command}`);
-    await $post("/ship/commands", { command })
-  });
-
-  window.setInterval(updateSpaceField, 1000);
-})
+<script>
+export function close() {
+	alert("Fechando...\n(Não implementado)")
+}
 </script>
 
 <style>
-#canvas {
-  height: calc(100vh - 4rem);
-  width: calc(100vw - 4rem);
-
-  padding: 2rem;
-
-  background-color: #36bbf5;
-  overflow: hidden;
-
-  position: relative;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-@keyframes slide {
-  0% {
-    transform: translate(1px);
-  }
-  50% {
-    transform: translate(-1px);
-  }
-  100% {
-    transform: translate(1px);
-  }
-}
-
-#deep-space {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  background-image: url("~/assets/space.png");
-  background-origin: content-box;
-  animation: slide 3s linear infinite;
-
-  position: absolute;
-  z-index: 0;
-}
-
-#space-field {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  position: relative;
-}
-
-.spaceship {
-  background-image: url("~/assets/spaceship.png");
-}
-
-.asteroid {
-  background-image: url("~/assets/asteroid.png");
-}
-
-.missile {
-  background-image: url("~/assets/missile.png");
-}
-
-.explosion {
-  background-image: url("~/assets/explosion.png");
-}
 </style>
